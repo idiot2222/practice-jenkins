c2bf5905f04b7151b2610cd3cc31a05670a8c581
idiot2222 <pky2892@gmail.com>
2022-12-29 11:25:55 +0900

    add hello api

--- src/main/java/me/bogeun/practicejenkins/controller/IndexController.java
+++ src/main/java/me/bogeun/practicejenkins/controller/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "Hello Jenkins!!!";
     }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello !!!!!!!";
+    }
 }
