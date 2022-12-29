8eab2be6e51e93272fca979559fa6893ed5dad3d
idiot2222 <pky2892@gmail.com>
2022-12-29 11:19:42 +0900

    init project

---
+++ src/main/java/me/bogeun/practicejenkins/PracticeJenkinsApplication.java
@@ -0,0 +1,13 @@
+package me.bogeun.practicejenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/bogeun/practicejenkins/PracticeJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bogeun.practicejenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
